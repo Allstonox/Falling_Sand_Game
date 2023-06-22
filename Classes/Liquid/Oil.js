201416b2206f131d477475b7b2d642f69a781da1
Allstonox <66976464+Allstonox@users.noreply.github.com>
2023-06-22 00:21:14 -0400

    Delete Classes directory

--- Classes/Liquid/Oil.js
+++
@@ -1,8 +0,0 @@
-class Oil extends Liquid {
-    constructor({ x, y, width, height, index, color = 'maroon', flammable = true }) {
-        super({ x, y, width, height, index, color, flammable });
-
-        this.burnChance = 0.5;
-    }
-
-}
--- Classes/Liquid/Water.js
+++
@@ -1,9 +0,0 @@
-class Water extends Liquid {
-    constructor({ x, y, width, height, index, color = 'lightseagreen', producesGas = true }) {
-        super({ x, y, width, height, index, color, producesGas });
-
-        this.gasToProduce = 'Steam';
-        this.vaporizeChance = 0.001;
-    }
-
-}
