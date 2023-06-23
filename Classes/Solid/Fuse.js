dc40699d071fdfa4e395448ca973931fc5b63339
Allstonox <66976464+Allstonox@users.noreply.github.com>
2023-06-23 18:59:34 -0400

    Delete Classes directory

--- Classes/Solid/Fuse.js
+++
@@ -1,8 +0,0 @@
-class Fuse extends Solid {
-    constructor({ x, y, width, height, index, color = {red: 139, green: 69, blue: 19}, dynamic = false, flammable = true }) {
-        super({ x, y, width, height, index, color, dynamic, flammable });
-
-        this.burnChance = 0.6;
-    }
-
-}
